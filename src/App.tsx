a75b05a00419243052198416a83be9f751498692
Xavier Lacey <xavier@loyaltylion.com>
2022-05-14 15:49:54 +0100

    fourth commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="full-name-container">
-          <h1 className="full-name">XAVIER LACEY</h1>
-        </div>
-        <h2>Software engineer</h2>
+        <h1 className="full-name">XAVIER LACEY</h1>
         <p>
           Hi I'm Xav, a software engineer living in London with five years' experience building
           great products specialising in Node.js, TypeScript and React.
