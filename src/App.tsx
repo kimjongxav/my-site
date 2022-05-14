508fcd146c45cded80aae5c829790bd3f3c1b0ce
Xavier Lacey <xavier@loyaltylion.com>
2022-05-14 14:25:08 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.scss'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Xavier Lacey</h1>
+        <h2>Software engineer</h2>
+        <p>I'm a software engineer with five years' experience working in ecommerce.</p>
+        <p>I'm comfortable working in TypeScript, JavaScript, Node.js, Ruby and Ruby on Rails..</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
