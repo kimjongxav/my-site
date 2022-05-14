bffdcd23f08d9c8f5da5f4c16d02b45408a267f4
Xavier Lacey <xavier@loyaltylion.com>
2022-05-14 18:31:11 +0100

    add links and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
 import './App.scss'
+import EmailSvg from './email.svg'
+import GithubSvg from './github.svg'
+import LinkedInSvg from './linkedin.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="full-name">XAVIER LACEY</h1>
-        <p>
-          Hi I'm Xav, a software engineer living in London with five years' experience building
-          great products specialising in Node.js, TypeScript and React.
-        </p>
-        <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/xlacey/">
-          linkedin
-        </a>
-        <a target="_blank" rel="noreferrer" href="https://github.com/kimjongxav/">
-          github
-        </a>
-      </header>
+    <div className="app">
+      <h1 className="full-name">
+        <span className="xav">XAV</span>IER LACEY
+      </h1>
+      <p>
+        Hi I'm Xav, a software engineer living in London with five years' experience building great
+        products specialising in Node.js, TypeScript and React.
+      </p>
+      <div className="links">
+        <div className="icons">
+          <a href="mailto:x@vierlacey.com" target="_blank" rel="noreferrer">
+            <img src={EmailSvg} alt="email" />
+          </a>
+          <a href="https://github.com/kimjongxav/" target="_blank" rel="noreferrer">
+            <img src={GithubSvg} alt="github" />
+          </a>
+          <a href="https://www.linkedin.com/in/xlacey/" target="_blank" rel="noreferrer">
+            <img src={LinkedInSvg} alt="linkedin" />
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
